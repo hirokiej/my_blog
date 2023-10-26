1f4f4feec0ee4a495d179ba85c2ca78fcdc01b8a
hirokiej <hiroblogdesu@gmail.com>
2023-10-25 20:20:03 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+    def index
+        @post = Post.all
+    end
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+      if @post.save
+        redirect_to posts_path, notice: "投稿されました"
+      else
+        render 'new'
+      end
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+        flash[:success]= "投稿を削除しました"
+    end
+
+    private
+
+    def post_params
+        params.require(:post).permit(:title,:content)
+    end
+
+end
